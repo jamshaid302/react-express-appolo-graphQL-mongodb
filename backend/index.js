5c4344431c8466179ea665f8138261908afbd9c7
M Jamshaid Khan <jkniazi302@gmail.com>
2024-02-15 23:05:34 -0800

    install packages

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(9000, () => {
+  console.log(`Server listen on port 9000`);
+});

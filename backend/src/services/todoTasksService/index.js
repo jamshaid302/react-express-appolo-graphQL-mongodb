2db502dc9c92589b84a1b24c6c02660f1064f399
M Jamshaid Khan <jkniazi302@gmail.com>
2024-02-21 23:38:32 -0800

    delete task and refactoring code

--- backend/src/services/todoTasksService/index.js
+++ backend/src/services/todoTasksService/index.js
@@ -5,13 +5,7 @@ const TodoTasksService = {
   async getTasks(args = {}) {
     try {
       const { where, take, skip, orderBy } = args || {};
-      const tasks = await db
-        .collection("tasks")
-        .find({
-          // _id: new ObjectId("65d45f3323f5edb5da1c1aad"),
-          title: where?.title,
-        })
-        .toArray();
+      const tasks = await db.collection("tasks").find({}).toArray();
       return tasks;
     } catch (error) {
       return error;
@@ -22,6 +16,17 @@ const TodoTasksService = {
     try {
       const task = await db.collection("tasks").insertOne({
         title: data?.title,
+      });
+      return task;
+    } catch (error) {
+      return error;
+    }
+  },
+
+  async deleteTask(id) {
+    try {
+      const task = await db.collection("tasks").deleteOne({
+        _id: new ObjectId(id),
       });
       return task;
     } catch (error) {
